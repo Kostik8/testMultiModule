2cf2c57b92508a2bd24cc9d8e2b31a5ecad0ecdc
Konstantin Daugavet <konstantin.daugavet@quartett-mobile.de>
2016-04-22 12:51:59 +0200

    first commit

---
+++ mylibrary/src/main/java/com/example/konstantin/mylibrary/Dummy.java
@@ -0,0 +1,12 @@
+package com.example.konstantin.mylibrary;
+
+/**
+ * <p>
+ * Support: <a href="mailto:audi.mmi-support@quartett-mobile.de">audi.mmi-support@quartett-mobile.de</a>
+ */
+public class Dummy {
+
+    public static int sum(int a, int b) {
+        return a - b;
+    }
+}
