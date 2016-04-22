2c9065c1d533c1629884f99f728c833b08da6452
Konstantin Daugavet <konstantin.daugavet@quartett-mobile.de>
2016-04-22 15:01:28 +0200

    header fix

--- mylibrary/src/main/java/com/example/konstantin/mylibrary/Dummy.java
+++ mylibrary/src/main/java/com/example/konstantin/mylibrary/Dummy.java
@@ -1,9 +1,5 @@
 package com.example.konstantin.mylibrary;
 
-/**
- * <p>
- * Support: <a href="mailto:audi.mmi-support@quartett-mobile.de">audi.mmi-support@quartett-mobile.de</a>
- */
 public class Dummy {
 
     public static int sum(int a, int b) {
