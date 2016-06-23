ae72e91cd09ad6b978978424e89ec865cef26d03
Konstantin Daugavet <konstantin.daugavet@quartett-mobile.de>
2016-06-23 15:30:34 +0200

    add publish source script

--- mylibrary/src/main/java/com/example/konstantin/mylibrary/Dummy.java
+++ mylibrary/src/main/java/com/example/konstantin/mylibrary/Dummy.java
@@ -1,8 +1,19 @@
 package com.example.konstantin.mylibrary;
 
+import android.support.annotation.NonNull;
+
+/**
+ * Wanna some javadoc for this class ??? update 1
+ */
 public class Dummy {
 
+    /**
+     * This is some strange method with stupid javadoc
+     * @return crazy string
+     */
+    @NonNull
     public static String getMessage() {
-        return "I'm source!";
+//        return "I'm source!";
+        return "I'm library!";
     }
 }
